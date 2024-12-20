1dfb01009f6f8d563b7b02c6996be9da33e4ee01
phtatdat03 <phamtatdat2703@gmail.com>
2024-12-20 18:06:49 +0700

    test

--- index.php
+++ index.php
@@ -8,6 +8,6 @@
 <body>
     <h1>Trang chủ cửa hàng</h1>
     <a href="admin/authen/login.php">Kiểm tra đơn hàng</a>
-
+    <p><input type="radio" name="fruit" value="quyt"/> Quýt</p>
 </body>
 </html>
