5933eeb5366d10f5f986af96a2f02cc68b8202ae
NguyenHuyCao <caohk001asd@gmail.com>
2025-02-20 20:57:17 +0700

    trang tin tức

---
+++ Blog_2.php
@@ -0,0 +1,176 @@
+
+<?php 
+ include("Layout/header.php");
+?>
+<!-- pages-title-start -->
+<section class="contact-img-area">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <div class="con-text">
+          <h2 class="page-title">TIN TỨC</h2>
+          <p>Nội Thất Cao Cấp - Khám Phá Vẻ Đẹp Sang Trọng Tại Luxury Home</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<body>
+    
+  <h2><i class="fas fa-star"></i> SỰ KẾT HỢP ĐỘC ĐÁO: LUXURY HOME X NHÀ THIẾT KẾ ĐỈNH CAO</h2>
+  <h5>Không chỉ là một dự án mang tính đột phá, đây còn là bước ngoặt quan trọng trong ngành thiết kế nội thất. Sự hợp tác này mang đến những xu hướng mới, giúp không gian sống trở nên tinh tế, hiện đại và đầy cảm hứng.</h5></br>
+  <div class="image">
+    <img src="images/Blog/Blog_2/ảnh1.1.jpg" />
+  </div>
+  <h5>Trong bộ sưu tập lần này, Luxury Home tập trung vào sự cân bằng giữa **tính thẩm mỹ** và **công năng sử dụng**. Các thiết kế sử dụng những chất liệu cao cấp như gỗ tự nhiên, đá cẩm thạch và kim loại mạ vàng, mang đến vẻ đẹp sang trọng và bền vững theo thời gian.</h5></br>
+  <div class="image">
+    <img src="images/Blog/Blog_2/ảnh2.2.png" />
+  </div>
+  <h5>Không gian phòng khách được thiết kế với phong cách hiện đại, kết hợp các gam màu trung tính để tạo nên sự hài hòa và ấm áp. Những chi tiết như **đèn chùm thủy tinh**, **ghế sofa bọc da cao cấp**, và **bàn trà đá cẩm thạch** giúp nâng tầm đẳng cấp cho căn hộ.</h5></br>
+  <div class="image">
+    <img src="images/Blog/Blog_2/ảnh3.3.jpg" />
+  </div>
+  <h5>Điểm nhấn đặc biệt trong bộ sưu tập là **bàn ăn Camo Dining**, được lấy cảm hứng từ sự giao thoa giữa cổ điển và hiện đại. Thiết kế mặt bàn đá nguyên khối kết hợp cùng chân kim loại tối giản mang đến vẻ đẹp tinh tế và bền vững.</h5></br>
+  <div class="image">
+    <img src="images/Blog/Blog_2/ảnh4.4.jpg" />
+  </div>
+  <h5>Bên cạnh đó, Luxury Home cũng giới thiệu mẫu **giường ngủ Monogram Bed**, được thiết kế với **chất liệu vải nhung cao cấp**, mang lại cảm giác êm ái và sang trọng. Đầu giường bo tròn với họa tiết monogram tinh tế, tạo điểm nhấn độc đáo cho không gian nghỉ ngơi.</h5></br>
+  <div class="image">
+    <img src="images/Blog/Blog_2/ảnh5.5.jpg" />
+  </div>
+  <h5>Bộ sưu tập còn bao gồm những món nội thất mang tính linh hoạt cao, như **ghế lounge đa năng**, **bàn làm việc phong cách Scandinavian**, giúp gia chủ dễ dàng tạo dựng không gian sống theo ý muốn.</h5></br>
+  <h6>Khám phá bộ sưu tập <a href="#">tại đây</a></h6>
+  <div class="image">
+    <img src="images/Blog/Blog_2/ảnh6.6.jpg" />
+  </div>
+
+  <!--------------------------------BÀI VIẾT LIÊN QUAN---------------------- -->
+  <hr  width="34%" align="center" /><!--đường kẻ ngăn cách giữa bài viết liên quan với ảnh trên -->
+  <h1>BÀI VIẾT LIÊN QUAN</h1>
+
+  <ul id="list-new">
+    <div class="item"><!--sản phẩm 2 -->
+      <img src="images/Blog/Blog_2/new1.jpg"width="345" height="345"  alt="">                   
+      <div class="name" >7 TIPS PHỐI ĐỒ VỚI VARSITY JACKET </div>
+      <div class="name" >THU HÚT MỌI ÁNH</div>
+    </div>  
+    <div class="box-left" >
+      <a href="Blog_1.php"><button>Xem thêm </button><!--nút mua hàng --></a>
+    </div>   
+    <div class="item"><!--sản phẩm 1 -->
+      <img src="images/Blog/Blog_2/new3.jpg"width="345" height="345"  alt="">                   
+      <div class="name">THÔNG TIN CHƯƠNG TRÌNH </div>
+      <div class="name">THẺ THÀNH VIÊN DIRTYCOINS</div>
+    </div>
+    <div class="box-left" >
+      <a href="Blog_3.php"><button>Xem thêm </button><!--nút mua hàng --></a>
+    </div>       
+  </ul>
+  <hr class="opacity-20">
+</body>
+<?php 
+include("Layout/footer.php"); 
+?>
+<style>
+  *{
+    margin: 0;
+    padding: 0;
+  }
+  i{/*  chỉnh icon ngôi sao */
+    font-size:16px;
+    text-align: center;
+    padding-right: 10px;
+  }
+
+  h2{/*  chỉnh ô chứa chữ H2 */
+    text-align: center;
+    font-size:23px; 
+    padding-left:325px;/*  chỉnh khoảng cách 2 bên lề để song song với ảnh */
+    padding-right:343px;
+    padding-bottom:70px;
+    padding-top:100px;
+  }
+
+  h5{/*  chỉnh ô chứa chữ H5 */
+    text-align: left;
+    font-size:16px; 
+    font-weight: 50;
+    padding-left:340px;/*  chỉnh khoảng cách 2 bên lề để song song với ảnh */
+    padding-right:343px;
+    padding-top:10px;
+    padding-bottom:18px;
+  }
+
+  h6{/*  chỉnh ô chứa chữ H6 */
+    text-align: left;
+    font-size:17.5px; 
+    font-weight: 600;
+    text-decoration: underline;
+    padding-left:355px;/*  chỉnh khoảng cách 2 bên lề để song song với ảnh */
+    padding-right:343px;
+    padding-bottom:10px;
+    margin-top:-10px;/*  chỉnh khoảng cách so với chữ bên trên */
+  }
+  .image{/*  chỉnh ảnh trong mục body */
+    align-items: center;
+    text-align: center;
+  }
+
+
+
+  /*-----------------BÀI VIẾT LIÊN QUAN--------------------------*/
+
+
+  hr{/*  chỉnh thanh kẻ giữa bài viết liên quan với ảnh trên */
+    margin-top:70px;/*  chỉnh khoảng cách so với chữ bên trên */padding-left:325px;/*  chỉnh khoảng cách 2 bên lề để song song với ảnh */
+    margin-left:313px;
+  }
+  h1{/*  chỉnh ô chứa chữ H1 */
+    text-align: left;
+    font-size:16px; 
+    font-weight: 550;
+    padding-left:325px;/*  chỉnh khoảng cách 2 bên lề để song song với ảnh */
+    padding-right:343px;
+    padding-bottom:18px;
+    margin-top:15px;/*  chỉnh khoảng cách so với chữ bên trên */
+  }
+  #list-new {/*  chỉnh ảnh bài viết liên quan */
+    font-size:10px;/*size chữ sản phẩm*/
+    display: flex;
+    list-style: none;
+    justify-content: space-around;
+    padding-left:190px;/*  chỉnh khoảng cách 2 bên lề để song song với ảnh */
+    padding-right:317px;
+    margin-top:40px;
+
+  }
+
+  #list-new .item .name {/*  chỉnh chữ bài viết liên quan */
+    text-align: center;
+    color:rgb(10, 10, 10);
+    font-weight: bold;
+    margin-top:20px;/*chỉnh khoảng cách từ tên so với sản phẩm*/
+  }
+
+
+  #list-new .box-left{
+    text-align: center;
+    margin-top:400px;/*chỉnh lên xuống nút xem thêm */
+    margin-left:-490px;/*chỉnh trái phải nút xem thêm*/
+  }
+  #list-new .box-left button:hover {/*màu sắc khi nhấp vô nút buttom mua ngay*/
+      background:orange;
+  }
+  #list-new .box-left button {/*nút buttom mua ngay*/
+      font-size:12px;/*chỉnh size chữ*/
+      width: 80px;/*chỉnh size dài bóng đen*/
+      height: 30px;/*chỉnh size rộng bóng đen*/
+      background:#1d1a1a;
+      border:none;
+      outline:none;
+      color:#fff;
+      font-weight: bold;
+      border-radius: 200px;
+      transition:0.4s;/*chỉnh tốc độ chuyển màu*/
+  }
+</style>
